225ffc0ec848b9523de5acce01c6d7aca583f064
Nicolae Ghimbovschi <xfreebird@gmail.com>
2017-01-26 17:23:07 +0200

    Update en_remove_keychain.rb

--- lib/fastlane/plugin/ciutils/actions/en_remove_keychain.rb
+++ lib/fastlane/plugin/ciutils/actions/en_remove_keychain.rb
@@ -5,10 +5,6 @@ module Fastlane
 
     class EnRemoveKeychainAction < Action
       def self.run(params)
-        # add original keychain to list
-        original = Actions.lane_context[Actions::SharedValues::ORIGINAL_DEFAULT_KEYCHAIN]
-        Fastlane::Actions.sh("security list-keychains -s #{original}", log: false) unless original.nil?
-
         begin
           keychain_name = Actions.lane_context[SharedValues::EN_KEYCHAIN_NAME]
           other_action.delete_keychain(name: keychain_name)
